ac672212d8f44d82edabaf61bc2e9f843fbcb185
Venkata Narasimhan <venkata.narasimhan@lightspeedhq.com>
2018-09-21 08:22:33 -0400

    Resetting the files into proper structure.

---
+++ src/test/java/commonfactory/CommonFactoryClass.java
@@ -0,0 +1,39 @@
+package commonfactory;
+
+import googlesetup.TestGoogleHomePageOpening;
+import org.codehaus.plexus.util.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CommonFactoryClass {
+
+    StringPlaceHolderClass spcObject = new StringPlaceHolderClass();
+
+    public HttpURLConnection responseData() throws IOException {
+        URL url = new URL(spcObject.sourceUrl);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        return connection;
+    }
+
+    public void screenShotMechanismOnFailure(ITestResult result){
+        if (result.getStatus() == ITestResult.FAILURE) {
+            TestGoogleHomePageOpening ocObject = new TestGoogleHomePageOpening();
+            System.out.println("A Test failure has occured, a screenshot has been taken");
+            File file = ocObject.driver.getScreenshotAs(OutputType.FILE);
+            try {
+                FileUtils.copyFile(file, new File("/Users/venkata.narasimhan/Documents/truefitrepo/truefitgoogle/screenshots/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(file.getAbsolutePath());
+        }
+    }
+}
+
