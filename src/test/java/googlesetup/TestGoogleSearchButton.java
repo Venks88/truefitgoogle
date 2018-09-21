f78413e1b41c8426e04faafcb1d4cf3ecf8110d9
Venkata Narasimhan <venkata.narasimhan@lightspeedhq.com>
2018-09-20 22:27:28 -0400

    Added support for BDD dev, using cucumber. Moved Folders. Adding tests for google button.

--- src/test/java/googlesetup/TestGoogleSearchButton.java
+++ src/test/java/googlesetup/TestGoogleSearchButton.java
@@ -1,5 +1,6 @@
 package googlesetup;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,7 +12,12 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class TestGoogleSearchButton extends TestListenerAdapter{
     private StringPlaceHolderClass sphObject = new StringPlaceHolderClass();
@@ -66,7 +72,7 @@ public class TestGoogleSearchButton extends TestListenerAdapter{
 
     //https://www.google.com/search?source=hp&q=Hello&btnK=Google+Search&oq=Hello
     @Test
-    public void test_SearchButtonWithText() throws InterruptedException {
+    public void test_SearchButtonWithText() {
         textSearchSetup("Hello");
         WebElement searchButton = ocObject.driver.findElementByName(sphObject.googleSearchButton);
         searchButton.submit();
@@ -82,33 +88,122 @@ public class TestGoogleSearchButton extends TestListenerAdapter{
     }
 
     @Test
-    public void test_SearchButtonWithNoText(){
-
+    public void test_SearchButtonWithNoText() throws InterruptedException{
+        textSearchSetup("");
+        WebElement searchButton = ocObject.driver.findElementByName(sphObject.googleSearchButton);
+        WebElement luckySearchButton = ocObject.driver.findElementByName(sphObject.googleSearchLuckyButton);
+        searchButton.submit();
+        luckySearchButton.submit();
+        Assert.assertFalse(searchButton.isDisplayed(),"Empty string searches work, when they should not be yeilding a new page");
+        Assert.assertTrue(searchButton.isEnabled(), "Empty string keeps the button in disabled state");
+        Assert.assertFalse(luckySearchButton.isDisplayed(),"Empty string searches work, when they should not be yeilding a new page");
+        Assert.assertTrue(luckySearchButton.isEnabled(), "Empty string keeps the button in disabled state");
     }
 
     @Test
     public void test_SearchButtonWithNumbers(){
-
+        ArrayList<String> inputArray = new ArrayList<String>();
+        Random rand = new Random();
+        for(int i = 0; i<=10; i++){
+            inputArray.add(Integer.toString(rand.nextInt()));
+        }
+        int length = inputArray.size();
+        for(int i=0; i<=length-1;i++){
+            textSearchSetup(inputArray.get(i));
+            WebElement searchButton = ocObject.driver.findElementByName(sphObject.googleSearchButton);
+            searchButton.submit();
+            WebDriverWait wait = new WebDriverWait(ocObject.driver, 4);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name(sphObject.googleTextFieldname)));
+            WebElement textFieldSrc = ocObject.driver.findElementByName(sphObject.googleTextFieldname);
+            Assert.assertEquals(textFieldSrc.getAttribute("value"), inputArray.get(i));
+            Assert.assertEquals(textFieldSrc.getAttribute("title"), "Search");
+            Assert.assertEquals(textFieldSrc.getAttribute("type"), "text");
+            Assert.assertEquals(textFieldSrc.getTagName(), "input");
+            Assert.assertTrue(textFieldSrc.isDisplayed(), "Text field is available on the redirected page");
+            Assert.assertTrue(textFieldSrc.isEnabled(), "Text field is NOT enabled");
+        }
     }
 
     @Test
     public void test_SearchButtonWithSpecialChars(){
-
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?";
+        ArrayList<String> inputArray = new ArrayList<String>();
+        for(int i = 0; i<=10; i++){
+            String specialCharString = RandomStringUtils.random( 15, characters );
+            inputArray.add(specialCharString);
+        }
+        int length = inputArray.size();
+        for(int i=0; i<=length-1;i++){
+            textSearchSetup(inputArray.get(i));
+            WebElement searchButton = ocObject.driver.findElementByName(sphObject.googleSearchButton);
+            searchButton.submit();
+            WebDriverWait wait = new WebDriverWait(ocObject.driver, 4);
+            WebElement textFieldSrc = ocObject.driver.findElementByName(sphObject.googleTextFieldname);
+            Assert.assertTrue(textFieldSrc.isDisplayed(), "Text field is available on the redirected page");
+            Assert.assertTrue(textFieldSrc.isEnabled(), "Text field is NOT enabled");
+            Assert.assertEquals(textFieldSrc.getAttribute("value"), inputArray.get(i));
+            Assert.assertEquals(textFieldSrc.getTagName(), "input");
+        }
     }
 
     @Test
     public void test_SearchButtonWithTextAndRedirect(){
-
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?";
+        ArrayList<String> inputArray = new ArrayList<String>();
+        String specialCharString = RandomStringUtils.random( 15, characters );
+        inputArray.add(specialCharString);
+        textSearchSetup(inputArray.get(0));
+        WebElement searchButton = ocObject.driver.findElementByName(sphObject.googleSearchButton);
+        searchButton.submit();
+        WebElement textFieldSrc = ocObject.driver.findElementByName(sphObject.googleTextFieldname);
+        Assert.assertTrue(textFieldSrc.isDisplayed(), "Text field is available on the redirected page");
+        Assert.assertTrue(textFieldSrc.isEnabled(), "Text field is NOT enabled");
+        ocObject.driver.navigate().back();
+        WebElement imgSource = ocObject.driver.findElement(By.xpath("//img[@id='hplogo']"));
+        Assert.assertTrue(imgSource.isDisplayed(), "Google Logo is displayed");
     }
 
     @Test
-    public void test_SearchButtonWithTextJSONResponse(){
-
+    public void test_SearchButtonWithTextJSONResponse() throws IOException {
+        Scanner s = new Scanner(new File("../../truefitrepo/truefitgoogle/src/test/java/searchInputText.txt"));
+        ArrayList<String> list = new ArrayList<String>();
+        while (s.hasNext()){
+            list.add(s.next());
+        }
+        s.close();
+        for(int i=0; i<=list.size()-1;i++) {
+            System.out.println("->  "+i);
+            URL url = new URL(sphObject.searchUrl +list.get(i)+sphObject.searchUrlClientPsy);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            Assert.assertEquals(connection.getResponseCode(),200);
+            Assert.assertEquals(connection.getResponseMessage(),"OK");
+            Assert.assertEquals(connection.getContentType(),"application/json; charset=ISO-8859-1");
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                Assert.assertTrue(inputLine.contains(list.get(i)),"The JSON content is invalid " + list.get(i) + inputLine);
+            in.close();
+        }
     }
 
     @Test
-    public void test_SearchButtonWithEmptyTextJSONResponse(){
-
+    public void test_SearchButtonWithEmptyTextFailTest() throws IOException{
+        URL url = new URL(sphObject.searchUrl +""+sphObject.searchUrlClientPsy);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        Assert.assertEquals(connection.getResponseCode(),200);
+        Assert.assertEquals(connection.getResponseMessage(),"OK");
+        Assert.assertEquals(connection.getContentType(),"application/json; charset=ISO-8859-1");
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                connection.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            Assert.assertTrue(inputLine.contains("{\"bpc\":false,\"tlw\":false}"),"The expected false notations are missing for empty search requests");
+        in.close();
     }
 
 }
